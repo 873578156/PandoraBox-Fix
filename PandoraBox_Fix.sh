950626cfa87c18a5f3ab73ab52cff8a539ecb32d
873578156 <873578156@qq.com>
2018-08-21 20:41:13 +0800

    Update PandoraBox_Fix.sh

--- PandoraBox_Fix.sh
+++ PandoraBox_Fix.sh
@@ -4,7 +4,7 @@ sed -i 's/http:\/\/downloads.pandorabox.com.cn\//http:\/\/pandorabox.leoslion.cn
 opkg update
 opkg install wget
 opkg install luci-i18n-shadowsocksr-pro-zh-cn
-wget --no-check-certificate https://github.com/873578156/Command-and-Conquer-Red-Alert-II-win10-patch/raw/master/RA2.INI
+wget --no-check-certificate https://github.com/873578156/PandoraBox_Fix/raw/master/README.md
 sed -i 's/config_get dnsmasqconfdir "$cfg" confdir "\/etc\/dnsmasq.d"/config_get dnsmasqconfdir "$cfg" confdir "\/tmp\/dnsmasq.d"/' /etc/init.d/dnsmasq
 sed -i 's/\/usr\/bin\/ip/ip/' /etc/init.d/ssrpro
 sed -i 's/\/etc\/init.d\/ssrpro.sh restart/\/etc\/init.d\/ssrpro restart/' /etc/shadowsocksr/up-gfwlist.sh
